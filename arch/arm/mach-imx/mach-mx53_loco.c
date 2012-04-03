f17d88104ba555f15568983fe15906a5a7a80719
Philipp Zabel <pza@pengutronix.de>
2012-04-03 17:18:40 +0200

    Enable SRTC support to MX51 BABBAGE and MX53 LOCO platforms
    Signed-off-by: Philipp Zabel <pza@pengutronix.de>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/mach-imx/mach-mx53_loco.c
+++ arch/arm/mach-imx/mach-mx53_loco.c
@@ -284,6 +284,7 @@ static void __init mx53_loco_board_init(void)
 	mx53_loco_fec_reset();
 	imx53_add_fec(&mx53_loco_fec_data);
 	imx53_add_imx2_wdt(0, NULL);
+	imx53_add_srtc();
 
 	ret = gpio_request_one(LOCO_ACCEL_EN, GPIOF_OUT_INIT_HIGH, "accel_en");
 	if (ret)
